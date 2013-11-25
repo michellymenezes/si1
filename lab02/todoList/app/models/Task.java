19275321d9d51aca18116f6aed75b7e04cb01aff
Michelly <michelly@ubuntu.(none)>
2013-11-24 22:06:25 -0300

    Deleção implementada

--- lab02/todoList/app/models/Task.java
+++ lab02/todoList/app/models/Task.java
@@ -2,25 +2,32 @@ package models;
 
 import java.util.*;
 
+import play.db.ebean.*;
 import play.data.validation.Constraints.*;
 
+import javax.persistence.*;
 
-public class Task {
-    
-  public Long id;
-  
-  @Required
-  public String label;
-  
-  
-  public static List<Task> all() {
-    return new ArrayList<Task>();
-  }
-  
-  public static void create(Task task) {
-  }
-  
-  public static void delete(Long id) {
-  }
-    
+@Entity
+public class Task extends Model {
+
+	@Id
+	public Long id;
+
+	@Required
+	public String label;
+	
+	public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
+
+	public static List<Task> all() {
+		return find.all();
+	}
+
+	public static void create(Task task) {
+		task.save();
+	}
+
+	public static void delete(Long id) {
+		find.ref(id).delete();
+	}
+
 }
